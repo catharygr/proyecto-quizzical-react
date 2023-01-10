9b9806914d6df6defe2a459d1b4c79c44aa2fcec
cathy <cathy@bubulazi.com>
2023-01-10 14:43:48 +0000

    Desarrollando radio en el formulario

--- quizzical-react-scrimba/src/componentes/Pregunta.jsx
+++ quizzical-react-scrimba/src/componentes/Pregunta.jsx
@@ -1,16 +1,41 @@
 import React from 'react' 
 
 export default function Pregunta({pregunta, respuestas, respCorrecta}) {
-  const [form, setForm] = React.useState('')
+  const [formData, setFormData] = React.useState({respuesta: ''})
 
-  const radios = respuestas.map(resp => <p key={resp}>{resp}</p> )
+  function ManejarCambio(e) {
+    const {name, value, type, checked} = e.target
+      setFormData(oldForm => ({...oldForm, respuesta: checked}))
+  }
+
+  const radios = respuestas.map(resp => {
+    return (
+      <>
+      <input 
+        key={resp}  
+        type="radio"
+        id={resp}
+        name='respuestas'
+        value={resp} 
+        onChange={ManejarCambio}
+        checked={formData.respuesta === resp}
+        />
+        <label htmlFor={resp}>
+          {resp}
+        </label>
+      </>
+    ) 
+  } )  
+    
 
   return (
     <div className='pregunta'>
-    <h3>{pregunta}</h3>
-    <div className='pregunta--respuestas'>
-    {radios}
-    </div>
+      <h3>{pregunta}</h3>
+      <form action="">
+        <fieldset className='pregunta--respuestas'>
+          {radios}
+        </fieldset>
+      </form>
     </div>
 
   )
